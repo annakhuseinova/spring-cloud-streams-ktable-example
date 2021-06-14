f842df94812372232f1de90996f0939e1bcd4532
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-14 13:44:07 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamsktableexample/SpringCloudStreamsKtableExampleApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsktableexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsKtableExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsKtableExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springcloudstreamsktableexample/SpringCloudStreamsKtableExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsktableexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsKtableExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
